34232791326f74d8433e6f2db350a3a5b1868c34
Eason Lu <aelnosu@outlook.com>
2024-01-27 15:06:22 -0800

    Init lexer.cpp and lexer.h wilth default config, change CMakeLists.txt to reflect the change and improve LLVM package.
    Signed-off-by: Eason Lu <aelnosu@outlook.com>

---
+++ lexer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by eason on 1/27/24.
+//
+
+#include "lexer.h"
---
+++ lexer.h
@@ -0,0 +1,8 @@
+//
+// Created by eason on 1/27/24.
+//
+
+#ifndef SCHEMEIMPROVED_LEXER_H
+#define SCHEMEIMPROVED_LEXER_H
+
+#endif //SCHEMEIMPROVED_LEXER_H
