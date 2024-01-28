10ba3fe8ff9e7928dc17b29b86c4342ec1246051
Eason Lu <aelnosu@outlook.com>
2024-01-27 16:25:26 -0800

    lexer complete
    Signed-off-by: Eason Lu <aelnosu@outlook.com>

--- main.cpp
+++ main.cpp
@@ -13,6 +13,16 @@ int main() {
                 (= this.age age))
             (method (toString)
                 (print "Name: " name ", Age: " age))))
+    (function main (void)
+        (Cow* cow1) ;this is a pointer not an obj
+        (Cow* cow2)
+
+        (= Cow1 (new Cow cow1 1)) ;this instantiate a cow obj on the heap
+        (= Cow2 (new Cow cow2 4))
+        (delete cow1)
+        (delete cow2)
+        (= Cow1 nullptr)
+        (= Cow2 nullptr))
     )";
 
     Lexer lexer(input);
